7a3c2c8b2e88c1e82683744c6d3587fb88566cf0
hmartinez <hmartinez@abae.gob.ve>
2023-03-12 21:19:27 -0400

    Version Estable 20230312 211927

--- scripts/generar_batchid.py
+++ scripts/generar_batchid.py
@@ -1,8 +1,14 @@
+import os
 from modulos.cplanxgen2_module import cplanxgen2
 from modulos.procexgen2_module import procexgen2
 from modulos.IDUpdate_module import ID_Update
 from modulos.cplanimport_module import cplanimport
+from ManageDB.sqlite_on_db import sqlite_Insertar_registro_masivo
 
+
+
+S_base_datos = 'vrss_operation_and_managment_subsystem'
+S_tabla = '`control_misiones_id_control_process`'
 
 print('{}% Extrayendo la tabla de misiones.'.format(int(0/8*100)))
 misiones_0 = cplanimport()
@@ -10,6 +16,19 @@ misiones_0 = cplanimport()
 dia_de_plan = input('{}% Introducir BatchID: '.format(int(1/8*100)))
 Date_Code_BatchID = int(dia_de_plan)
 
+# Aqui generamos el CPLAN
 cplanxgen2(misiones_0, Date_Code_BatchID)
+# Aqui generamos el resto de los archivos y construimos el arbol de directorios
 procexgen2()
-ID_Update(Date_Code_BatchID)
+# Aqui se construye el dataframe de procesos
+df = ID_Update(Date_Code_BatchID)
+print(df)
+
+pregunta = input('Desea actualizar la tabla de procesos? (S/N): ')
+
+if pregunta == 's' or pregunta == 'S':
+    try:
+        os.chdir('../..')
+        sqlite_Insertar_registro_masivo(S_base_datos, S_tabla, df, 0, 4)
+    except:
+        print('No se actualizo la tabla!')
--- scripts/gensteps/idupdate_exec.py
+++ scripts/gensteps/idupdate_exec.py
@@ -5,8 +5,5 @@ dia_de_plan = input('{}% Introducir BatchID: '.format(int(0/8*100)))
 
 Date_Code_BatchID = int(dia_de_plan)
 
-try:
-    df = ID_Update(Date_Code_BatchID)
-    print(df)
-except:
-    print('Directorio No encontrado!')
+df = ID_Update(Date_Code_BatchID)
+print(df)

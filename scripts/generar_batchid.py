23894eb3cdec4374a5edf0b6c26d1db609717c40
hmartinez <hmartinez@abae.gob.ve>
2023-03-18 21:48:15 -0400

    Version Estable 20230318 214814

--- scripts/generar_batchid.py
+++ scripts/generar_batchid.py
@@ -4,11 +4,13 @@ from modulos.processes.procexgen2_module import procexgen2
 from modulos.processes.IDUpdate_module import ID_Update
 from modulos.builders.cplanimport_module import cplanimport
 from ManageDB.sqlite_on_db import sqlite_Insertar_registro_masivo
+from modulos.processes.routing_module import routing
 from General_Utilities.control_rutas import setting_routes
 
 
-key = 'databases'
-directorio = setting_routes(key)[0]
+key = 'database'
+mode = False
+directorio = routing(mode)[key]
 S_base_datos = directorio + 'vrss_operation_and_managment_subsystem'
 S_tabla = '`control_misiones_id_control_process`'
 

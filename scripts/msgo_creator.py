fa05cf1cdbc5df02e07d5cc76820a0a6624bd4ea
hmartinez <hmartinez@abae.gob.ve>
2023-04-16 18:13:22 -0400

    Version Estable 20230416 181322

--- scripts/msgo_creator.py
+++ scripts/msgo_creator.py
@@ -2,26 +2,30 @@ from ManageDB.sqlite_on_db import *
 from General_Utilities.fecha import format_FechaID
 from modulos.msgservice.msgo_creator_module import general_msgo_sender
 from modulos.processes.routing_module import routing
+from General_Utilities.fecha import format_FechaID, DeltaT
 
 
 key = 'database'
 mode = False
 database = routing(mode)[key] + 'vrss_operation_and_managment_subsystem'
-Fecha = input('Introduzca la fecha: ')
-Fecha = format_FechaID(Fecha)
-
 # key = 'tables'
 # tables = setting_routes(key)
 # table = option_list(tables)
 table = 'guardias'
 
-# print(table)
+fecha = input('Introduzca la fecha: ')
+ahora = format_FechaID(fecha)
 
-List = [
-    'Id',
-    'planificador',
-    'Apoyo',
-    'dia_planificacion'
-]
+for i in range(4):
+    j = 7 * i
+    print(DeltaT(ahora, -j))
+    Fecha = DeltaT(ahora, -j)
 
-general_msgo_sender(database, table, Fecha, List)
+    List = [
+        'Id',
+        'planificador',
+        'Apoyo',
+        'dia_planificacion'
+    ]
+
+    general_msgo_sender(database, table, Fecha, List)

bfd84726149f34feccb37970f4bfc8979452c03f
hmartinez00 <hectoralonzomartinez00@gmail.com>
2023-07-27 09:46:51 -0400

    Version Estable 20230727 094650

--- modulos/Package_update/git_update.py
+++ modulos/Package_update/git_update.py
@@ -3,8 +3,9 @@ from Pku_module.Package_update_module import auto_commit
 from General_Utilities.option_list import option_list
 from Pku_module.Package_update_module import listar_paquetes
 
-os.chdir('..')
-opciones = os.listdir()
-project = option_list(opciones)
+# os.chdir('..')
+# opciones = os.listdir()
+# project = option_list(opciones)
 
-auto_commit(project)
+# auto_commit(project)
+auto_commit()
